ea7a801c8ce265298962f17ffb70d6b225a3940f
snowsf <1493636205@qq.com>
2021-02-02 19:16:06 +0800

    change email

--- weather.sh
+++ weather.sh
@@ -2,13 +2,13 @@
 
 set -eux
 
-CITY={Guangzhou,Zhuzhou}
+CITY={Zhuzhou,Guangzhou}
 LANGUAGE="zh-CN"
 UNIT=m
-UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
+UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"
 
 curl \
   -H "Accept-Language: $LANGUAGE" \
   -H "User-Agent: $UA" \
   -o result.html \
-  wttr.in/$CITY?format=4\&$UNIT
+  wttr.in/$CITY
