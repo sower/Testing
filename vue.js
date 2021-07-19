ad14924c04196384c296d9930b0197ddb6d3ace7
snowsf <1493636205@qq.com>
2021-07-19 23:56:46 +0800

    change

--- vue.js
+++ vue.js
@@ -1,15 +1,16 @@
 'use strict'
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+// import { createApp } from 'vue'
+// import App from './App.vue'
+// import router from './router'
+// import store from './store'
 
-import Antd from "ant-design-vue";
-import "ant-design-vue/dist/antd.css";
+// import Antd from "ant-design-vue";
+// import "ant-design-vue/dist/antd.css";
 
-const app = createApp(App);
-app.config.productionTip = false;
+// const app = createApp(App);
+// app.config.productionTip = false;
 
-app.use(store).use(router).use(Antd).mount('#app')
+// app.use(store).use(router).use(Antd).mount('#app')
 
+console.log('sucessfully run!');
